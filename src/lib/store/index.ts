dd7d814382f58048e70fe2ee407ea15e6ea3da55
sovlookup <gonorth@qq.com>
2021-07-27 01:01:57 +0800

    等待pouchdb和vite兼容

--- src/lib/store/index.ts
+++ src/lib/store/index.ts
@@ -1,21 +1,2 @@
-import { addPouchPlugin, createRxDatabase, getRxStoragePouch, removeRxDatabase } from 'rxdb'
-import * as ldb from 'pouchdb-adapter-leveldb'
-
-addPouchPlugin(ldb)
-let ddb
-// create the database with the storage creator.
-const db = async () => {
-	ddb = ddb
-		? ddb
-		: await createRxDatabase({
-				name: 'mydata',
-				storage: getRxStoragePouch('leveldb'),
-				ignoreDuplicate: true
-		  })
-	return ddb
-}
-
-console.log(db())
-
 export * from './appState'
 export * from './windowState'
