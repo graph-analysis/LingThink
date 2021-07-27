088962cdc2c523e462f4ee1007c17a18daa4f8aa
sovlookup <gonorth@qq.com>
2021-07-28 00:52:13 +0800

    feat: 自定义store

--- src/lib/store/store.ts
+++ src/lib/store/store.ts
@@ -18,55 +18,55 @@ interface GlobalConfig {
 
 const subscriber_queue = []
 
-function configStore(ref: any, methods = {}) {
-	let store = {}
-	const subscribers = []
+// function configStore(ref: any, methods = {}) {
+// 	let store = {}
+// 	const subscribers = []
 
-	// Add a listener to GUN data
-	ref.on(function (data: {}, key: string | number) {
-		/* If the ref._get matches the data key it means we are getting
-		 * data from a call to gun.get(), and so we don't need the store
-		 * to be an object with nested data. Otherwise we are getting data
-		 * from a call to map() and should nest the data in an object
-		 */
-		if (ref._.get === key) {
-			store = data
-		} else if (!data) {
-			/* This clause will not work as intended on null values / false / 0
-			 * if you use such data consider subscribing to a parent node instead
-			 * eg. gun.get("temperature") instead of gun.get("temperature").get("value").
-			 * Or you can pass a validate() function (TODO: add example)
-			 */
-			delete store[key]
-		} else {
-			store[key] = data
-		}
-		// Tell each subscriber that data has been updated
-		for (let i = 0; i < subscribers.length; i += 1) {
-			subscribers[i](store)
-		}
-	})
+// 	// Add a listener to GUN data
+// 	ref.on(function (data: {}, key: string | number) {
+// 		/* If the ref._get matches the data key it means we are getting
+// 		 * data from a call to gun.get(), and so we don't need the store
+// 		 * to be an object with nested data. Otherwise we are getting data
+// 		 * from a call to map() and should nest the data in an object
+// 		 */
+// 		if (ref._.get === key) {
+// 			store = data
+// 		} else if (!data) {
+// 			/* This clause will not work as intended on null values / false / 0
+// 			 * if you use such data consider subscribing to a parent node instead
+// 			 * eg. gun.get("temperature") instead of gun.get("temperature").get("value").
+// 			 * Or you can pass a validate() function (TODO: add example)
+// 			 */
+// 			delete store[key]
+// 		} else {
+// 			store[key] = data
+// 		}
+// 		// Tell each subscriber that data has been updated
+// 		for (let i = 0; i < subscribers.length; i += 1) {
+// 			subscribers[i](store)
+// 		}
+// 	})
 
-	function subscribe(subscriber: (arg0: any) => void) {
-		subscribers.push(subscriber)
+// 	function subscribe(subscriber: (arg0: any) => void) {
+// 		subscribers.push(subscriber)
 
-		// 推送初始状态
-		subscriber(store)
+// 		// 推送初始状态
+// 		subscriber(store)
 
-		// 在组件卸载时移除订阅
-		return () => {
-			const index = subscribers.indexOf(subscriber)
-			if (index !== -1) {
-				subscribers.splice(index, 1)
-			}
-			if (!subscribers.length) {
-				ref.off()
-			}
-		}
-	}
+// 		// 在组件卸载时移除订阅
+// 		return () => {
+// 			const index = subscribers.indexOf(subscriber)
+// 			if (index !== -1) {
+// 				subscribers.splice(index, 1)
+// 			}
+// 			if (!subscribers.length) {
+// 				ref.off()
+// 			}
+// 		}
+// 	}
 
-	return { ...methods, subscribe }
-}
+// 	return { ...methods, subscribe }
+// }
 
 export function writable<T>(
 	ref: any,
