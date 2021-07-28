dfab74c29f3abf6a670579fba9902826de1e0ee7
sovlookup <gonorth@qq.com>
2021-07-29 02:38:27 +0800

    feat: store in gun

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess'
-import adapter from '@sveltejs/adapter-node'
+import adapter from '@sveltejs/adapter-static'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,14 +10,21 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
+		// todo: 在 GUNstore 加载完毕之前先使用临时储存
 		adapter: adapter({
-			out: 'build',
-			precompress: true,
-			env: {
-				host: '0.0.0.0',
-				port: '3000'
-			}
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null
 		})
+		// adapter: adapter({
+		// 	out: 'build',
+		// 	precompress: true,
+		// 	env: {
+		// 		host: '0.0.0.0',
+		// 		port: '3000'
+		// 	}
+		// })
 	}
 }
 
