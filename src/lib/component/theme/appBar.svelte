0638909cdd4386bdded3a365a862ee3a921b943b
sovlookup <gonorth@qq.com>
2021-08-02 23:19:31 +0800

    feat: 完善国际化体验

--- src/lib/component/theme/appBar.svelte
+++ src/lib/component/theme/appBar.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
 	import AppBar from './default/appBar.svelte'
+	import { getLocaleFromNavigator, init } from 'svelte-i18n'
 	import type { Store } from '$lib/store'
 	export let store: Store
 
+	const { remoteStore } = store
+	const { userStore } = remoteStore
 	// 加载国际化资源
 	import './i18n'
 	import { isLoading } from 'svelte-i18n'
+
+	// 初始化语言模块
+	init({
+		fallbackLocale: 'zh',
+		// 加载语言配置
+		initialLocale: $userStore.config.windowConfig.language
+	})
 </script>
 
 <!-- 国际化资源加载完毕 -->
--- src/lib/component/theme/i18n/index.ts
+++ src/lib/component/theme/i18n/index.ts
@@ -1,11 +1,5 @@
 import { getLocaleFromNavigator, init, register } from 'svelte-i18n'
 
 // 按需加载语言包
-register('zh', () => import('./zh.json'))
-register('en', () => import('./en.json'))
-
-// 初始化语言模块
-init({
-	fallbackLocale: 'zh',
-	initialLocale: getLocaleFromNavigator()
-})
+register('zh', () => import('./zh_CN.json'))
+register('en', () => import('./en_US.json'))
