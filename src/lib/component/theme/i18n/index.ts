debafe1832080e788562ec56d169f3ae8d161a17
sovlookup <gonorth@qq.com>
2021-08-02 23:20:37 +0800

    feat: 完善国际化体验

--- src/lib/component/theme/i18n/index.ts
+++ src/lib/component/theme/i18n/index.ts
@@ -1,5 +1,6 @@
 import { register } from 'svelte-i18n'
 
 // 按需加载语言包
-register('zh', () => import('./zh_CN.json'))
-register('en', () => import('./en_US.json'))
+// 注册的 key 为 Language 枚举类型的值
+register('中文简体', () => import('./zh_CN.json'))
+register('English', () => import('./en_US.json'))
